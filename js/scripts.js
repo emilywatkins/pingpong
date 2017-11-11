783b83f0571ef69148c63013cc7bbb5c034885fa
Emily Watkins <emily.e.dolliver@gmail.com>
2017-11-10 21:39:28 -0800

    move countArray function to back end logic and add printArray function - forEach loop to format and append array items to html list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
-countUp = function(number) {
+var countArray = function(input1) {
+  var array = [];
+  for (var i = 1; i <= input1; i += 1) {
+    array.push(i);
+  }
+  return array;
+}; //add forEach loop that prints each array item into ul.
 
-}
+var printArray = function(numbers) {
+  numbers.forEach(function(number) {
+    var list = $("<li>" + number + "</li>");
+    $(".output").append(list);
+    console.log(list);
+  });
+};
+
 
 $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
 
-    var input1 = $("#input1").val();
-    var array = [];
-    for (var i = 1; i <= input1; i += 1) {
-      array.push(i);
-      debugger;
-    }
+    var input1 = parseInt($("#input1").val());
+    var numbers = countArray(input1);
+    printArray(numbers);
 
+    $(".output").show();
 
-    console.log(array);
-
-  })
-})
+  });
+});
