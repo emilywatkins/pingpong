a788a6f50bf165707c12d11fa43c9bedbc9d76e0
Emily Watkins <emily.e.dolliver@gmail.com>
2017-11-10 18:52:27 -0800

    add for loop to create array of all numbers 1 to input number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+countUp = function(number) {
 
+}
 
 $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
 
-  
+    var input1 = $("#input1").val();
+    var array = [];
+    for (var i = 1; i <= input1; i += 1) {
+      array.push(i);
+      debugger;
+    }
+
+
+    console.log(array);
+
   })
 })
